c6ebcfaf749c398955a804fbb30788201095b389
TestItEver <94520813+TestItEver@users.noreply.github.com>
2021-11-19 12:57:59 +0100

    Added some functions

--- sandbox/src/main/java/julijak/first/MyFirstProgram.java
+++ sandbox/src/main/java/julijak/first/MyFirstProgram.java
@@ -3,7 +3,27 @@ package julijak.first;
 public class MyFirstProgram {
 
    public static void main(String[] args) {
-      System.out.println("Hello, World");
+      hello("world");
+      hello("Alex");
+
+      double l = 5;
+      System.out.println("Area of the square with side length " + l + " = " + area(l));
+
+      double a = 3;
+      double b = 7;
+      System.out.println("Area of the rectangle with side lengths " + a + " and " + b + " = " + area(a,b));
+   }
+
+   public static void hello(String w){
+      System.out.println("Hello, " + w + "!");
+   }
+
+   public static double area(double len){
+      return len * len;
+   }
+
+   public static double area(double a, double b){
+      return a * b;
    }
 
 }
