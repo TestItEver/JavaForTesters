49d4e12057e0dfcc43a6dd69ab3c77127e7ef48f
TestItEver <94520813+TestItEver@users.noreply.github.com>
2021-11-19 10:19:11 +0100

    Changed format settings

--- sandbox/src/main/java/julijak/first/MyFirstProgram.java
+++ sandbox/src/main/java/julijak/first/MyFirstProgram.java
@@ -2,7 +2,7 @@ package julijak.first;
 
 public class MyFirstProgram {
 
-   public static void main(String[] args){
+   public static void main(String[] args) {
       System.out.println("Hello, World");
    }
 
