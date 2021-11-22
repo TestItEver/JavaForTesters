f9a5e5459a0a8bedf57232dfa1af839b8aeb5497
TestItEver <94520813+TestItEver@users.noreply.github.com>
2021-11-22 08:34:09 +0100

    Created two new classes

--- sandbox/src/main/java/julijak/first/MyFirstProgram.java
+++ sandbox/src/main/java/julijak/first/MyFirstProgram.java
@@ -1,29 +1,36 @@
 package julijak.first;
 
+import org.w3c.dom.css.Rect;
+
 public class MyFirstProgram {
 
    public static void main(String[] args) {
       hello("world");
       hello("Alex");
 
-      double l = 5;
-      System.out.println("Area of the square with side length " + l + " = " + area(l));
+      Square s = new Square(5);
+      //s.l = 5;
+      System.out.println("Area of the square with side length " + s.l + " = " + s.area());
 
-      double a = 3;
-      double b = 7;
-      System.out.println("Area of the rectangle with side lengths " + a + " and " + b + " = " + area(a,b));
+      Rectangle r = new Rectangle(3,7);
+      //r.a = 3;
+      //r.b = 7;
+      System.out.println("Area of the rectangle with side lengths " + r.a + " and " + r.b + " = " + r.area());
    }
 
    public static void hello(String w){
       System.out.println("Hello, " + w + "!");
    }
 
-   public static double area(double len){
-      return len * len;
+   /*
+   public static double area(Square s){
+      return s.l * s.l;
    }
 
-   public static double area(double a, double b){
-      return a * b;
+   public static double area(Rectangle r){
+      return r.a * r.b;
    }
+
+    */
 
 }
