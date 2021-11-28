b6c94ca92703097e42313162c31be183c2104528
TestItEver <94520813+TestItEver@users.noreply.github.com>
2021-11-28 20:08:59 +0100

    Completed second step for the 2-level test architecture

--- tests-addressbook-web/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ tests-addressbook-web/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -5,61 +5,32 @@ import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
+
    public WebDriver wd;
+
+   private NavigationHelper navigationHelper;
+   private GroupHelper groupHelper;
+   private SessionHelper sessionHelper;
 
    public void init() {
       wd = new ChromeDriver();
       // wd = new FirefoxDriver();
       wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
       wd.get("http://localhost/addressbook/addressbook/");
-      login("admin", "secret");
+      groupHelper = new GroupHelper(wd);
+      navigationHelper = new NavigationHelper(wd);
+      sessionHelper = new SessionHelper(wd);
+      sessionHelper.login("admin", "secret");
    }
 
-   private void login(String username, String password) {
-     wd.findElement(By.name("user")).click();
-     wd.findElement(By.name("user")).clear();
-     wd.findElement(By.name("user")).sendKeys(username);
-     wd.findElement(By.name("pass")).click();
-     wd.findElement(By.name("pass")).clear();
-     wd.findElement(By.name("pass")).sendKeys(password);
-     wd.findElement(By.xpath("//input[@value='Login']")).click();
-   }
+
 
    public void logout() {
      wd.findElement(By.linkText("Logout")).click();
-   }
-
-   public void returnToGroupPage() {
-     wd.findElement(By.linkText("group page")).click();
-   }
-
-   public void submitGroupForm() {
-     wd.findElement(By.name("submit")).click();
-   }
-
-   public void fillGroupForm(GroupData groupData) {
-     wd.findElement(By.name("group_name")).click();
-     wd.findElement(By.name("group_name")).clear();
-     wd.findElement(By.name("group_name")).sendKeys(groupData.getGroupname());
-     wd.findElement(By.name("group_header")).click();
-     wd.findElement(By.name("group_header")).clear();
-     wd.findElement(By.name("group_header")).sendKeys(groupData.getGroupheader());
-     wd.findElement(By.name("group_footer")).click();
-     wd.findElement(By.name("group_footer")).clear();
-     wd.findElement(By.name("group_footer")).sendKeys(groupData.getGroupfooter());
-   }
-
-   public void initGroupCreation() {
-     wd.findElement(By.name("new")).click();
-   }
-
-   public void gotoGroupPage() {
-     wd.findElement(By.linkText("groups")).click();
    }
 
    public void stop() {
@@ -84,11 +55,16 @@ public class ApplicationManager {
      }
    }
 
-   public void deleteSelectedGroups() {
-     wd.findElement(By.xpath("//div[@id='content']/form/input[5]")).click();
+   public GroupHelper getGroupHelper() {
+      return groupHelper;
    }
 
-   public void selectGroup() {
-     wd.findElement(By.name("selected[]")).click();
+   public NavigationHelper getNavigationHelper() {
+      return navigationHelper;
    }
+
+   public SessionHelper getSessionHelperHelper() {
+      return sessionHelper;
+   }
+
 }
--- tests-addressbook-web/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ tests-addressbook-web/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -7,11 +7,11 @@ public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
-    app.gotoGroupPage();
-    app.initGroupCreation();
-    app.fillGroupForm(new GroupData("Test1", "Simply Test", "Tadaa"));
-    app.submitGroupForm();
-    app.returnToGroupPage();
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().initGroupCreation();
+    app.getGroupHelper().fillGroupForm(new GroupData("Test1", "Simply Test", "Tadaa"));
+    app.getGroupHelper().submitGroupForm();
+    app.getGroupHelper().returnToGroupPage();
     app.logout();
   }
 
