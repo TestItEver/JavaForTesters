a634fb64e671e9bc0535b30b38292447a5af2b59
TestItEver <94520813+TestItEver@users.noreply.github.com>
2021-11-29 09:34:17 +0100

    Modified ContactHelper (extends HelperBase now)

--- tests-addressbook-web/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ tests-addressbook-web/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -12,38 +12,25 @@ public class ContactHelper extends HelperBase {
    }
 
    public void initContactCreation() {
-      wd.findElement(By.linkText("add new")).click();
+      click(By.linkText("add new"));
    }
 
    public void submitContactForm() {
-      wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+      click(By.xpath("//div[@id='content']/form/input[21]"));
    }
 
    public void fillContactData(ContactData contactData) {
-      wd.findElement(By.name("firstname")).click();
-      wd.findElement(By.name("firstname")).clear();
-      wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
-      wd.findElement(By.name("lastname")).click();
-      wd.findElement(By.name("lastname")).clear();
-      wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
-      wd.findElement(By.name("company")).click();
-      wd.findElement(By.name("company")).clear();
-      wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
-      wd.findElement(By.name("mobile")).click();
-      wd.findElement(By.name("mobile")).clear();
-      wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
-      wd.findElement(By.name("bday")).click();
+      type(By.name("firstname"),contactData.getFirstname());
+      type(By.name("lastname"), contactData.getLastname());
+      type(By.name("company"), contactData.getCompany());
+      type(By.name("mobile"), contactData.getMobile());
+      click(By.name("bday"));
       new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactData.getBday());
-      wd.findElement(By.name("bmonth")).click();
+      click(By.name("bmonth"));
       new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getBmonth());
-      wd.findElement(By.name("byear")).click();
-      wd.findElement(By.name("byear")).click();
-      wd.findElement(By.name("byear")).clear();
-      wd.findElement(By.name("byear")).sendKeys(contactData.getByear());
-      wd.findElement(By.name("email")).click();
-      wd.findElement(By.name("email")).clear();
-      wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
-      wd.findElement(By.name("new_group")).click();
+      type(By.name("byear"), contactData.getByear());
+      type(By.name("email"), contactData.getEmail());
+      click(By.name("new_group"));
       new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getGroup());
    }
 }
