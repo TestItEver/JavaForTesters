33fd1d9ae4da63967b09b2406fde5d8f992a76bc
TestItEver <94520813+TestItEver@users.noreply.github.com>
2021-12-03 20:56:12 +0100

    Added if-else for different browsers (Firefix, Chrome, IE)

--- tests-addressbook-web/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ tests-addressbook-web/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,20 +2,36 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
+import javax.sql.rowset.BaseRowSet;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
+
    public WebDriver wd;
 
    private ContactHelper contactHelper;
    private NavigationHelper navigationHelper;
    private GroupHelper groupHelper;
    private SessionHelper sessionHelper;
+   String browser;
+
+   public ApplicationManager(String browser) {
+      this.browser = browser;
+   }
 
    public void init() {
-      wd = new ChromeDriver();
-      // wd = new FirefoxDriver();
+      if (browser.equals(BrowserType.FIREFOX)) {
+         wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.CHROME)) {
+         wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.IE)) {
+         wd = new InternetExplorerDriver();
+      }
+
       wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
       wd.get("http://localhost/addressbook/");
       groupHelper = new GroupHelper(wd);
