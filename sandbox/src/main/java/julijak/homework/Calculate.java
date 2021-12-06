c0d8b452161a2eac588a3db4b9e9319ef2d93fb0
TestItEver <94520813+TestItEver@users.noreply.github.com>
2021-12-06 11:22:38 +0100

    added some examples

--- sandbox/src/main/java/julijak/homework/Calculate.java
+++ sandbox/src/main/java/julijak/homework/Calculate.java
@@ -1,12 +1,59 @@
 package julijak.homework;
 
+import jdk.nashorn.internal.objects.NativeString;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Calculate {
    public static void main(String[] args) {
+      HashSet<Character> s = new HashSet<>();
+      s.add('a');
+      s.add('d');
+      s.add('f');
+      System.out.println(s);
+
+      s.add('a');
+      s.add('e');
+      s.add('e');
+      s.remove('a');
+      s.remove('c');
+
+      System.out.println(s);
+
+      HashMap<String, String> myMap = new HashMap<>();
+      myMap.put("Detektive", "Duke");
+      System.out.println(myMap);
+      myMap.put("Detektive", "Duke");
+      System.out.println(myMap);
+      System.out.println(myMap.size());
+
+      System.out.println(rekursion("ALLA"));
+
       Point p1 = new Point(-1,-1);
       Point p2 = new Point(2, 2);
 
       System.out.println("Distance calculated with function is: " + distance(p1,p2));
       System.out.println("Distance calculated with class method is: " + p1.distance(p2));
+
+      int[] arr = new int[5];
+      arr[0] = 1;
+      arr[1] = 2;
+      arr[2] = 2;
+      arr[3] = 33;
+      arr[4] = 1;
+
+      for (int i : arr){
+         int anz = 0;
+         for (int j : arr){
+            if (i == j) {
+               anz++;
+            }
+         }
+         if (anz == 1) {
+            System.out.println(i);
+         }
+      }
 
    }
 
@@ -18,4 +65,15 @@ public class Calculate {
       y = Math.abs(p1.y - p2.y);
       return Math.sqrt(x * x + y * y);
    }
+
+   private static boolean rekursion(String wort){
+      if (wort.length()<=1) {
+         return true;
+      }
+      if (wort.charAt(0) == wort.charAt(wort.length()-1)) {
+         return rekursion(wort.substring(1, wort.length()-1));
+      }
+      return false;
+   }
 }
+
