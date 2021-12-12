b574fff7c0de73ed8cfd1e6963e576ef32738420
TestItEver <94520813+TestItEver@users.noreply.github.com>
2021-12-12 14:57:54 +0100

    Added sort of the lists

--- tests-addressbook-web/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ tests-addressbook-web/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -37,6 +39,12 @@ public class GroupCreationTests extends TestBase {
     group.setId(max);
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));
+
+    // other way: comparison of sorted lists
+    Comparator<? super GroupData> byId = (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(after, before);
 
     app.getSessionHelper().logout();
   }
