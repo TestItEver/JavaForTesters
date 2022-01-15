25332765896b85e969ab374fbdeae0bd4cedbe7a
TestItEver <94520813+TestItEver@users.noreply.github.com>
2022-01-15 20:09:18 +0100

    Added changes for lessons 8.3+8.4

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -12,13 +12,15 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.MatchResult;
 
 public class ApplicationManager {
 
    private final Properties properties;
-   public WebDriver wd;
+   private WebDriver wd;
 
    String browserType;
+   private RegistrationHelper registrationHelper;
 
    public ApplicationManager(String browser) {
       this.browserType = browser;
@@ -28,23 +30,43 @@ public class ApplicationManager {
    public void init() throws IOException {
       String target = System.getProperty("target", "local");
       properties.load(new FileReader(new File(String.format("./src/test/resources/%s.properties", target))));
-
-      if (browserType.equals(BrowserType.FIREFOX)) {
-         wd = new FirefoxDriver();
-      } else if (browserType.equals(BrowserType.CHROME)) {
-         wd = new ChromeDriver();
-      } else if (browserType.equals(BrowserType.IE)) {
-         wd = new InternetExplorerDriver();
-      } else if (browserType.equals(BrowserType.EDGE)) {
-         wd = new EdgeDriver();
-      }
-
-      wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-      wd.get(properties.getProperty("web.baseURL"));      // "http://localhost/addressbook/";
    }
 
    public void stop() {
-      wd.quit();
+      if (wd != null) {
+         wd.quit();
+      }
    }
 
+   public HttpSession newSession() {
+      return new HttpSession(this);
+   }
+
+   public String getProperty(String key) {
+      return properties.getProperty(key);
+   }
+
+   public RegistrationHelper registration() {
+      if (registrationHelper == null) {                           // lazy initialization (only if necessary)
+         registrationHelper = new RegistrationHelper(this);
+      }
+      return registrationHelper;
+   }
+
+   public WebDriver getDriver() {
+      if (wd == null) {                                        // lazy initialization (only if necessary)
+         if (browserType.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+         } else if (browserType.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+         } else if (browserType.equals(BrowserType.IE)) {
+            wd = new InternetExplorerDriver();
+         } else if (browserType.equals(BrowserType.EDGE)) {
+            wd = new EdgeDriver();
+         }
+         wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+         wd.get(properties.getProperty("web.baseURL"));      // "http://localhost/addressbook/";
+      }
+      return wd;
+   }
 }
