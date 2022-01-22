3fec55a3110b744b5f31f48d8eb1c22b8995338c
TestItEver <94520813+TestItEver@users.noreply.github.com>
2022-01-22 20:05:37 +0100

    Replaced parameters with getProperty from local.properties

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/SoapHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/SoapHelper.java
@@ -22,27 +22,27 @@ public class SoapHelper {
 
    public Set<Project> getProjects() throws RemoteException, MalformedURLException, ServiceException {
       MantisConnectPortType mc = getMantisConnect();
-      ProjectData[] projects = mc.mc_projects_get_user_accessible("administrator", "root");
+      ProjectData[] projects = mc.mc_projects_get_user_accessible(app.getProperty("web.adminLogin"), app.getProperty("web.adminPassword"));
       return Arrays.asList(projects).stream().map((p) ->
                       new Project().withId(p.getId().intValue()).withName(p.getName())).collect(Collectors.toSet());
    }
 
    private MantisConnectPortType getMantisConnect() throws ServiceException, MalformedURLException {
       MantisConnectPortType mc = new MantisConnectLocator().
-              getMantisConnectPort(new URL("http://localhost/mantisbt-2.25.2/api/soap/mantisconnect.php"));
+              getMantisConnectPort(new URL(app.getProperty("web.soapURL")));
       return mc;
    }
 
    public Issue addIssue(Issue issue) throws MalformedURLException, ServiceException, RemoteException {
       MantisConnectPortType mc = getMantisConnect();
-      String[] categories = mc.mc_project_get_categories("administrator", "root", BigInteger.valueOf(issue.getProject().getId()));
+      String[] categories = mc.mc_project_get_categories(app.getProperty("web.adminLogin"), app.getProperty("web.adminPassword"), BigInteger.valueOf(issue.getProject().getId()));
       IssueData issueData = new IssueData();
       issueData.setSummary(issue.getSummary());
       issueData.setDescription(issue.getDescription());
       issueData.setProject(new ObjectRef(BigInteger.valueOf(issue.getProject().getId()), issue.getProject().getName()));
       issueData.setCategory(categories[0]);
-      BigInteger issueId = mc.mc_issue_add("administrator", "root", issueData);
-      IssueData createdIssueData = mc.mc_issue_get("administrator", "root", issueId);
+      BigInteger issueId = mc.mc_issue_add(app.getProperty("web.adminLogin"), app.getProperty("web.adminPassword"), issueData);
+      IssueData createdIssueData = mc.mc_issue_get(app.getProperty("web.adminLogin"), app.getProperty("web.adminPassword"), issueId);
       return new Issue().withId(createdIssueData.getId().intValue())
               .withSummary(createdIssueData.getSummary())
               .withDescription(createdIssueData.getDescription())
